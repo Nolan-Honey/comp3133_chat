3dde762f82a2a4f3ed7ca485e0a8b5afde1ea177
Rawr <All-Cashmere@users.noreply.github.com>
2019-04-17 15:48:56 -0400

    socket.js changed

--- app.js
+++ app.js
@@ -3,26 +3,24 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+var bParser = require('body-parser')
 var port = process.env.PORT || 5000;
 require('./socketManager/socket')(io);
 
 mongoose.Promise = global.Promise;
 //db connection
-mongoose.connect('mongodb://chat:admin123@ds048368.mlab.com:48368/chat', { useNewUrlParser: true }, (err)=> {
+mongoose.connect('mongodb://chat:admin123@ds048368.mlab.com:48368/chat', { useNewUrlParser: true }, (e)=> {
 
-    if (err){
-        console.log(err);
+    if (e){
+        console.log(e);
     }
     else {
         console.log('db is connected...');
     }
 });
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+app.use(bParser.urlencoded({ extended: true }));
+app.use(bParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -35,5 +33,5 @@ app.use('/', require('./routes/rooms'));
 
 //port connection
 server.listen(port, function(){
-    console.log('Listening on port ' + port);
+    console.log('Listening on' + port);
 });
--- socketManager/socket.js
+++ socketManager/socket.js
@@ -2,8 +2,8 @@ var rooms = []
 var users = {}
 var User = require('../models/Users.js');
 var Chat = require('../models/Chats.js');
-var Sockio = require('../models/Sockets.js');
-var Elog = require('../models/Events.js');
+var S = require('../models/Sockets.js');
+var EventLog = require('../models/Events.js');
 var Private = require('../models/PrivateChats.js');
 var Rooms = require('../models/Rooms');
 
@@ -15,56 +15,53 @@ module.exports = (io)=>{
             }
             res.forEach((i)=>{
                 if (rooms.includes(i.room)){
-                    console.log('Lets not add this one: ' + i.room)
+                    console.log(i.room + 'already exists.')
                 }else{
-                    console.log('lets add this one: ' + i.room)
+                    console.log(i.room + 'can be added.')
                     rooms.push(i.room)
                 }
             })
         });
     //store event
-        var connectEvent=new Elog({type:'CONNECTION', socket:socket.id, room:'Lobby'})
-        connectEvent.save((err)=>{
-                if (err) throw err;
-                     console.log('\n==========STORE EVENT IN DATABASE==========\nSocket: '+connectEvent.socket+'\nWith type: '+connectEvent.type+"\nHas been connected @: "+ connectEvent.connect +'\nIn the: '+connectEvent.room+'\nSaved to database at: '+ connectEvent.connect)
+        var connectEvent=new EventLog({type:'CONNECTION', socket:socket.id, room:'Lobby'})
+        connectEvent.save((e)=>{
+                if (e) throw e;
+                     console.log('\n Event socket '+ connectEvent.socket +' type '+connectEvent.type+"\n connected at "+ connectEvent.connect +'  room name '+connectEvent.room+' database saved at'+ connectEvent.connect)
             })
         socket.on('NEW_USER',  (data ,callback) =>{ 
-        //if a user exists in the dictionary, return an error message to client
+        //if a user exists in the dictionary, return an eor message to client
             if (data in users){
                 callback(false);
             }
             else {
-            //if a new user exits, create one, store in dictionary
+            //Create new one and store user in dictionary if exists
                 callback(true);
                 socket.nickname = data;
                 users[socket.nickname] = socket;
                 updateNicknames();
-            //store in database
+            //username in db
                 var newUser = new User({username: data})
-                newUser.save((err)=>{
-                    if (err) throw err;
-                console.log('\n==========STORE USER IN DATABASE==========\nUser: '+newUser.username+"\nSaved to database")
+                newUser.save((e)=>{
+                    if (e) throw e;
+                console.log('\nUser '+newUser.username+" saved to db.")
                 })
-            //stores new socket
-                var newSock=new Sockio({socket_id:socket.id, createdBy:newUser.username})
-                newSock.save((err)=>{
-                    if (err) throw err;
-                    console.log('\n==========STORE SOCKET IN DATABASE==========\nSocket: '+newSock.socket_id+"\nCreated by: "+ newSock.createdBy+"\nSaved to database at: "+ newSock.connectTime)
+            //new socket in db
+                var newS=new S({socket_id:socket.id, createdBy:newUser.username})
+                newS.save((e)=>{
+                    if (e) throw e;
+                    console.log('\nSocket id '+newS.socket_id+" created by "+ newS.createdBy+" saved to db at "+ newS.connectTime)
                 })
-            //store event
-                var newUserEvent=new Elog({type:'NEW USER',name:newUser.username, socket:socket.id, room:'Main Room'})
-                newUserEvent.save((err)=>{
-                    if (err) throw err;
-                    console.log('\n==========STORE EVENT IN DATABASE==========\nEvent Type: '+newUserEvent.type+'\nCreated by: ' + newUserEvent.name + '\nFor Socket: '+newUserEvent.socket+'\nIn the: '+newUserEvent.room+'\nSaved to database at: '+ newUserEvent.connect)
+            //event in db
+                var newUserEvent=new EventLog({type:'NEW USER',name:newUser.username, socket:socket.id, room:'Main Room'})
+                newUserEvent.save((e)=>{
+                    if (e) throw e;
+                    console.log('\nEvent type '+newUserEvent.type+'created by ' + newUserEvent.name + 'for socket '+newUserEvent.socket+' in the room'+newUserEvent.room+'saved to db at '+ newUserEvent.connect)
                 })
                 socket.room = 'Main';
-            // add the users's username to the global list
-            // send user to Main room
+            //adds username to the global list and sends them to the main room
                 socket.join('Main');
-            // echo to user they've connected
-            // echo to Main room that another user has connected to their room
-                message=({author:'CHAT BOT NINJA', message: 'You have connected to Main room'})
-                message2=({author:'CHAT BOT NINJA', message: socket.nickname + ' has connected to this room'})
+                message=({author:'Chat admin', message: 'You have connected to Main room'})
+                message2=({author:'Chat admin', message: socket.nickname + ' has connected to this room'})
                 socket.emit('UPDATE_CHAT', message);
                 socket.broadcast.to('Main').emit('UPDATE_CHAT', message2);
                 socket.emit('UPDATE_ROOMS', rooms, 'Main');
@@ -76,46 +73,45 @@ module.exports = (io)=>{
         }
 
 
-        //save messages to the database
+        //save messages to the db
         socket.on('SEND_MESSAGE',  (data)=> {
-            var newMessageEvent=new Elog({type:'MESSAGE SENT', name:socket.nickname, socket:socket.id, room:data['room']})
-            newMessageEvent.save((err)=>{
-                if (err) throw err;
-                console.log('\n==========STORE EVENT IN DATABASE==========\nEvent Type: '+newMessageEvent.type+'\nCreated by: ' + newMessageEvent.name + '\nFor Socket: '+newMessageEvent.socket+'\nIn the: '+newMessageEvent.room+'\nSaved to database at: '+ newMessageEvent.connect)
+            var newMessageEvent=new EventLog({type:'MESSAGE SENT', name:socket.nickname, socket:socket.id, room:data['room']})
+            newMessageEvent.save((e)=>{
+                if (e) throw e;
+                console.log('Event type '+newMessageEvent.type+'created by ' + newMessageEvent.name + 'for socket '+newMessageEvent.socket+' in the room '+newMessageEvent.room+'saved to db at '+ newMessageEvent.connect)
             })
-            //var msg = data.trim();
             var newMsg = new Chat({msg: data['message'], nick: socket.nickname, room: data['room']})
-            newMsg.save( (err) =>{
-                if (err) throw err;
-                console.log('\n==========STORE MESSAGE IN DATABASE==========\nMessage: '+newMsg.msg+'\nSent by: ' + newMsg.nick + '\nIn Room: '+newMsg.room)
+            newMsg.save( (e) =>{
+                if (e) throw e;
+                console.log('Message "'+newMsg.msg+'" by ' + newMsg.nick + 'from room '+newMsg.room)
                 io.sockets.in(socket.room).emit('NEW_MESSAGE', {author:socket.nickname, message:data['message']})
             })
         })
-        //handle the switching of rooms
+        //switch room
         socket.on('SWITCH_ROOM', (newroom)=>{
             socket.leave(socket.room);
         //store leave room event
-            var leaveRoomEvent=new Elog({type:'LEAVE ROOM', name:socket.nickname, socket:socket.id, room:socket.room})
-            leaveRoomEvent.save((err)=>{
-                if (err) throw err;
-                console.log('\n==========STORE EVENT IN DATABASE==========\nEvent Type: '+leaveRoomEvent.type+'\nCreated by: ' + leaveRoomEvent.name + '\nFor Socket: '+leaveRoomEvent.socket+'\nIn the: '+leaveRoomEvent.room+'\nSaved to database at: '+ leaveRoomEvent.connect)
+            var leaveRoomEvent=new EventLog({type:'LEAVE ROOM', name:socket.nickname, socket:socket.id, room:socket.room})
+            leaveRoomEvent.save((e)=>{
+                if (e) throw e;
+                console.log('\nEvent type '+leaveRoomEvent.type+'created by ' + leaveRoomEvent.name + 'for socket '+leaveRoomEvent.socket+' in the room '+leaveRoomEvent.room+'saved to db at '+ leaveRoomEvent.connect)
             })
             socket.join(newroom);
         //store join room event
-            var joinRoomEvent=new Elog({type:'JOIN ROOM', name:socket.nickname, socket:socket.id, room:newroom})
-            joinRoomEvent.save((err)=>{
-                if (err) throw err;
-                console.log('\n==========STORE EVENT IN DATABASE==========\nEvent Type: '+joinRoomEvent.type+'\nCreated by: ' + joinRoomEvent.name + '\nFor Socket: '+joinRoomEvent.socket+'\nIn the: '+joinRoomEvent.room+'\nSaved to database at: '+ joinRoomEvent.connect)
+            var joinRoomEvent=new EventLog({type:'JOIN ROOM', name:socket.nickname, socket:socket.id, room:newroom})
+            joinRoomEvent.save((e)=>{
+                if (e) throw e;
+                console.log('\nEvent Type '+joinRoomEvent.type+'created by ' + joinRoomEvent.name + 'for socket '+joinRoomEvent.socket+'in the room '+joinRoomEvent.room+'saved to db at '+ joinRoomEvent.connect)
             })
-            message3=({author:'CHAT BOT NINJA', message: 'You have connected to ' + newroom})
+            message3=({author:'Chat admin', message: 'You have connected to ' + newroom})
             socket.emit('UPDATE_CHAT', message3);
         // sent message to old room
-            message4=({author:'CHAT BOT NINJA', message: socket.nickname+' has left this room'})
+            message4=({author:'Chat admin', message: socket.nickname+' has left this room'})
             socket.broadcast.to(socket.room).emit('UPDATE_CHAT', message4 );
         // update socket session room title
             socket.room = newroom;
         //let users know new user has joined the room
-            message5=({author:'CHAT BOT NINJA', message: socket.nickname+'  has joined this room'})
+            message5=({author:'Chat admin', message: socket.nickname+'  has joined this room'})
             socket.broadcast.to(newroom).emit('UPDATE_CHAT', message5);
             socket.emit('UPDATE_ROOMS', rooms, newroom);
         });
@@ -124,26 +120,26 @@ module.exports = (io)=>{
         if (!socket.nickname) return;
         //remove username from dictionary to allow its reuse
         delete  users[socket.nickname];
-            Sockio.find({socket_id:socket.id},(err,socks)=>{
-                if (err) throw err;
+            S.find({socket_id:socket.id},(e,socks)=>{
+                if (e) throw e;
                 //update disconnect time for socket in database 
                 socks.forEach((sock)=> { 
                     sock.disconnectTime=new Date();
                     //save the update
-                    sock.save((err)=>{
-                        if (err) throw err;
-                        console.log( "\n==========UPDATE SOCKET DISCONNECT IN DATABASE==========\nSocket_id: " + sock.socket_id + "\nNew disconnectTime: " + sock.disconnectTime + "\nSAVED" );
+                    sock.save((e)=>{
+                        if (e) throw e;
+                        console.log( "\nSocket id " + sock.socket_id + "disconnected timeline " + sock.disconnectTime);
                     })
                 })
             })
         //store disconnect event
-            var disconnectEvent=new Elog({type:'DISCONNECT', disconnect: new Date(), name:socket.nickname, socket:socket.id})
-            disconnectEvent.save((err)=>{
-                if (err) throw err;
-                console.log('\n==========STORE EVENT IN DATABASE==========\nEvent Type: '+disconnectEvent.type+'\nCreated by: ' + disconnectEvent.name + '\nFor Socket: '+disconnectEvent.socket+'\nSaved to database at: '+ disconnectEvent.disconnect)
+            var disconnectEvent=new EventLog({type:'DISCONNECT', disconnect: new Date(), name:socket.nickname, socket:socket.id})
+            disconnectEvent.save((e)=>{
+                if (e) throw e;
+                console.log('\nEvent type '+disconnectEvent.type+' created by ' + disconnectEvent.name + ' for socket '+disconnectEvent.socket+' saved to db at '+ disconnectEvent.disconnect)
             })
         //let other users in the room know user has disconnected
-            message6=({author:'CHAT BOT NINJA', message: socket.nickname+' has disconnected'})
+            message6=({author:'Chat admin', message: socket.nickname+' has disconnected'})
             socket.broadcast.emit('UPDATE_CHAT', message6);
             socket.leave(socket.room);
             updateNicknames();
