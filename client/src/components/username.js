a74470ad410cc50d8d91f1d8eb22df83151da0dd
All-Cashmere <dakyung934@gmail.com>
2019-04-19 15:14:58 -0400

    Some changes on username.js

--- client/src/components/username.js
+++ client/src/components/username.js
@@ -2,19 +2,17 @@ import React from "react";
 import { Button, FormGroup, FormControl } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../resources/style.css';
-
-
 class Username extends React.Component {
 
 
     render() { 
         return (
             <div>
-                <form onSubmit={this.props.submit}>
+                <form onSubmit={this.props.submit} >
                 <span>
                     Chat Login
                 </span>
-                <FormGroup controlId="username">
+                <FormGroup controlId="username" >
                     <label>Nickname</label>
                     <FormControl autoFocus value={this.props.user} onChange={this.props.change}/>
                 </FormGroup>
